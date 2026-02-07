ccc476638cf52cece118d99715999d87fc427078
Admin <admin@MAC926-22.local>
2026-02-07 14:50:17 +0700

    newrt5

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,16 +1,16 @@
-import { View , TextInput, Button, StyleSheet ,Text,TouchableOpacity} from "react-native";
-import{useState, useEffect} from "react";
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage"
 type Book = {
-    id : string,
+    id: string,
     name: string,
     price: string
 }
 
-export default function Add(){
-    const[bookName,setBookName] = useState("")
-    const[bookPrice, setBookPrice] = useState("")
-    const[allBook, setAllBook] = useState<Book[]>([])
+export default function Add() {
+    const [bookName, setBookName] = useState("")
+    const [bookPrice, setBookPrice] = useState("")
+    const [allBook, setAllBook] = useState<Book[]>([])
 
     useEffect(() => {
         loadBook()
@@ -18,16 +18,16 @@ export default function Add(){
 
     async function loadBook() {
         const data = await AsyncStorage.getItem("book")
-        if(data !== null){
+        if (data !== null) {
             setAllBook(JSON.parse(data))
         }
     }
 
-    async function addbook(){
-        const book ={
-            id : Date.now().toString(),
+    async function addbook() {
+        const book = {
+            id: Date.now().toString(),
             name: bookName,
-            price: bookPrice                                                
+            price: bookPrice
         }
 
         console.log(book)
@@ -40,35 +40,35 @@ export default function Add(){
         setBookPrice("")
     }
 
-    return(
+    return (
         <View >
-            <Text style={{fontSize:20}}>ชื่อหนังสือ</Text>
-            <TextInput value={bookName} onChangeText={setBookName} 
-            style={mystyle.input}/>
-            <Text style={{fontSize:20}}>ราคาหนังส์อ</Text>
-            <TextInput value={bookPrice} onChangeText={setBookPrice} 
-            style={mystyle.input}/>
-            <TouchableOpacity  style={mystyle.Btn} onPress={() => addbook()}>
-                 <Text style={{color:"#ffffff",fontSize:20}}>บันทึก</Text>
+            <Text style={{ fontSize: 20 }}>ชื่อหนังสือ</Text>
+            <TextInput value={bookName} onChangeText={setBookName}
+                style={mystyle.input} />
+            <Text style={{ fontSize: 20 }}>ราคาหนังส์อ</Text>
+            <TextInput value={bookPrice} onChangeText={setBookPrice}
+                style={mystyle.input} />
+            <TouchableOpacity style={mystyle.Btn} onPress={() => addbook()}>
+                <Text style={{ color: "#ffffff", fontSize: 20 }}>บันทึก</Text>
             </TouchableOpacity>
-          
+
         </View>
     )
 }
 
 const mystyle = StyleSheet.create({
-    input:{
-        width:"100%",
+    input: {
+        width: "100%",
         borderWidth: 2
     },
-    
-    Btn:{
-        borderWidth:1,
-        backgroundColor:"black",
-        width:"100%",
-        height:30,
-        marginTop:10,
-        alignItems:"center"
+
+    Btn: {
+        borderWidth: 1,
+        backgroundColor: "black",
+        width: "100%",
+        height: 30,
+        marginTop: 10,
+        alignItems: "center"
     }
 
 })
